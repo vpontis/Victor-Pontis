eafda9d6ac48a5377a8152b2789dc9224bb88acd
Victor Pontis <vpontis@gmail.com>
2013-09-17 00:00:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VictorPontis::Application.config.secret_key_base = '7d4b615be4737f7271b6a0b424efbc1b79e8607aaa886656d5d764e9ed02a26d421a79bd0e14638903a26387450fbf35b598a295f384b264ba7c0bc4b02ec13e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VictorPontis::Application.config.session_store :cookie_store, key: '_victor_pontis_session'
